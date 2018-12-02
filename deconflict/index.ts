2787a650953c8426aba0b5f1bcdfc3f54ca4df58
Danny <danny@nulleffort.com>
2018-12-01 22:30:38 -0500

    Cleanup algorithm functional

---
+++ deconflict/index.ts
@@ -0,0 +1,80 @@
+import { DocumentScope } from "nano";
+
+
+interface IDeleteObj {
+	_id: string
+	_rev: string
+	_deleted: boolean
+}
+
+
+// This function takes the list of revisions and removes any deleted or not 'ok' ones.
+// returns a flat array of document objects
+const filterList = (list, excludeRev?) => {
+	const retval = [];
+	for (const i in list) {
+		if (!list.hasOwnProperty(i)) {
+			return retval;
+		}
+		if (list[i].ok && !list[i].ok._deleted) {
+			if (!excludeRev || (excludeRev && list[i].ok._rev != excludeRev)) {
+				// @ts-ignore
+				retval.push(list[i].ok);
+			}
+		}
+	}
+	return retval;
+};
+
+// convert the incoming array of document to an array of deletions - {_id:"x",_rev:"y",_deleted:true}
+const convertToDeletions = (list): IDeleteObj[] => {
+	const retval = [];
+	for (const i in list) {
+		if (!list.hasOwnProperty(i)) {
+			return retval;
+		}
+		const obj = { _id: list[i]._id, _rev: list[i]._rev, _deleted: true };
+		// @ts-ignore
+		retval.push(obj);
+	}
+	return retval;
+};
+
+/**
+ * Algorithm to determine winner by latest update.
+ * @param {nano.DocumentScope<any>} db
+ * @param {string} docId
+ * @param {string} fieldName
+ * @returns {Promise<any>}
+ */
+export const latestWins = (db: DocumentScope<any>, docId: string, fieldName: string): Promise<any> => {
+	// fetch the document with open_revs=all
+	// @ts-ignore
+	return db.get(docId, { open_revs: "all" }).then((data) => {
+
+		// remove 'deleted' leaf nodes from the list
+		const docList = filterList(data);
+
+		// if the there is only <=1 revision left, the document is either deleted
+		// or not conflicted; either way, we're done
+		if (docList.length <= 1) {
+			return Promise.reject("Document is not conflicted.");
+		}
+
+		// sort the array of documents by the supplied fieldName
+		// our winner will be the last object in the sorted array
+		const docListSorted = [...docList].sort((a, b) => {
+			return a[fieldName] - b[fieldName]
+		});
+		console.log("docListSorted::", docListSorted);
+		// turn the remaining leaf nodes into deletions
+		const docListDeletes: IDeleteObj[] = convertToDeletions(docListSorted.slice(0, -1));
+
+		return docListDeletes;
+		// now we can delete the unwanted revisions
+		// return db.bulk({ docs: docListDeletes });
+
+	}).catch((err) => {
+		console.error("Document could not be fetched");
+	});
+};
--- index.ts
+++ index.ts
@@ -1,3 +1,58 @@
-const testing = setInterval(() => {
-	console.log("TEST::", +(new Date()));
-}, 3000);
+import Nano, {
+	DocumentGetResponse,
+	DocumentListParams,
+	DocumentListResponse,
+	DocumentResponseRow,
+	DocumentScope,
+	ServerScope
+} from "nano"
+import { latestWins } from "./deconflict";
+
+const AUTH: string = Buffer.from("admin:hkHM0Hut78HEe9TyLg6e").toString("base64");
+const DB_URLS = {
+	local: "http://couch_acdb:5984",
+	remote: "https://acdbapi.com/"
+};
+
+
+/*               DB Refs
+ ========================================= */
+
+const localCouch: ServerScope = Nano(DB_URLS.local);
+const remoteCouch: ServerScope = Nano({
+	url: DB_URLS.remote,
+	requestDefaults: {
+		headers: {
+			"Authorization": `Basic ${AUTH}`
+		}
+	}
+});
+const testDB: DocumentScope<any> = remoteCouch.db.use("determineifapinisaclosematchtothehighlightedregion");
+
+
+// const testing = setInterval(() => {
+// 	console.log("ts::", +(new Date()));
+// }, 10000);
+
+remoteCouch.db.list().then((body) => {
+	return body.forEach((db) => {
+		console.log(db);
+	});
+}).then(() => {
+	const params: DocumentListParams = {
+		conflicts: true,
+		include_docs: true
+	};
+	testDB.list(params).then((body: DocumentListResponse<any>) => {
+		body.rows.forEach((doc: DocumentResponseRow<any>) => {
+			// output each document's body
+			if (doc.doc._conflicts && doc.doc._conflicts.length) {
+				return latestWins(testDB, doc.doc._id, "updated")
+					.then((res: DocumentGetResponse) => {
+						console.log("latestWins::res::", res, "\n");
+					});
+			}
+			return {};
+		});
+	});
+}).catch(console.error);
